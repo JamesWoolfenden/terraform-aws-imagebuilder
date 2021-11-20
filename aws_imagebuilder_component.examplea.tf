5e6a89942c11555f029c282a3c6d1189976b4829
James Woolfenden <James.Woolfenden@gmail.com>
2021-11-20 16:52:48 +0000

    update

--- aws_imagebuilder_component.examplea.tf
+++ aws_imagebuilder_component.examplea.tf
@@ -1,8 +1,9 @@
 resource "aws_imagebuilder_component" "examplea" {
-  data     = var.component
-  name     = var.component_name
-  platform = var.platform
-  version  = var.component_version
+  data       = var.component
+  name       = var.component_name
+  platform   = var.platform
+  version    = var.component_version
+  kms_key_id = var.kms_key.arn
   //supported_os_versions = ["Linux"]
   tags = var.common_tags
 }
